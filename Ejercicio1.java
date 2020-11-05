0581eb79b9de742ed08a1bab2718885f8d6793f9
Pool <paul.luque.19@gmail.com>
2020-11-04 21:30:59 -0500

    de forma iterativa

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		Game game = new Game();
-		game.prepararCartas();
-		game.playGame();
+		Scanner scan = new Scanner(System.in);
+		String again;
+		Game game;
+		do {
+			game = new Game();
+			game.prepararCartas();
+			game.playAGame();
+			System.out.print("\n\nPlay another game (y/n)?: ");
+			again = scan.nextLine();
+		} while (again.equals("y"));
 	}
 
 }
